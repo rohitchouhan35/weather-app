d129d56c97eb1b628398ece2eed67c0b878941b4
rohit <rohitchouhan3512@gmail.com>
2023-06-26 23:18:34 +0530

    test

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Vapi';
+const url = "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Vapi";
+
 const options = {
-	method: 'GET',
-	headers: {
-		'content-type': 'application/octet-stream',
-		'X-RapidAPI-Key': 'd8de83bd88msh12ae0fef0181fbfp1f9bdbjsn31f5a7826477',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "content-type": "application/octet-stream",
+    "X-RapidAPI-Key": "f90807da6dmsh845b6818bec4fddp1efb41jsn6e261d6eddd1",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
+
+// --------- JSON RESPONSE -------
+
 async function city() {
-    const searchText = "Delhi";
-    document.getElementById("searchForm").addEventListener("submit", async function(event) {
-        event.preventDefault(); // Prevent form submission
-      
-        // Retrieve the value from the input field
-        const searchInput = document.getElementById("searchInput");
-        const searchText = searchInput.value;
-        
-        // Do something with the search text
-        // console.log("Search Text: " + searchText);
-        // You can use the searchText for further processing, such as sending it to a server for search functionality, etc.
-        
-        const newUrl =  url.replace('Vapi', searchText);
-        // console.log(newUrl);
-        
-        try {
-            const response = await fetch(newUrl, options);
-            const result = await response.text();
-            console.log(result);
-            const weatherData = JSON.parse(result);
-            document.getElementById("temp").textContent = weatherData.temp;
-        } catch (error) {
-            console.error(error);
-        }
+  const searchText = "Delhi";
+  document
+    .getElementById("searchForm")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault(); // Prevent form submission
+      const searchInput = document.getElementById("searchInput");
+      const searchText = searchInput.value;
+
+      // Retrieve the value from the input field
+
+      // Do something with the search text
+      // console.log("Search Text: " + searchText);
+      // You can use the searchText for further processing, such as sending it to a server for search functionality, etc.
+
+      const newUrl = url.replace("Vapi", searchText);
+      // console.log(newUrl);
+
+      try {
+        const response = await fetch(newUrl, options);
+        const result = await response.text();
+        console.log(result);
+        const weatherData = JSON.parse(result);
+        document.getElementsByClassName("temp-text")[0].textContent = weatherData.temp;
+        document.getElementsByClassName("temp-text")[0].style.fontSize = "48px";
+        console.log(weatherData.temp);
+        console.log(document.getElementsByClassName("temp-text").textContent);
+      } catch (error) {
+        console.error(error);
+      }
     });
 
-    console.log("start");
-    return searchText;
+  console.log("start");
+  return searchText;
 }
- 
+
 city();
+
+
